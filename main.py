70239eb31de651990fe1c9d0d741ad6f3af340e7
aybarito <94233001+aybarito@users.noreply.github.com>
2023-09-24 17:38:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-# main.py
-
+import hashlib
 import time
-from block import Block, Blockchain
-from tree import MerkleTree
+from block import Block, Blockchain, MerkleTree
 
 def main():
-    blockchain = Blockchain()
-    tree = MerkleTree()
+    blockchain = Blockchain()  # Rename the variable
 
     while True:
         print("1. Add transactions")
         print("2. View blockchain")
-        print("3. Verify blockchain")
-        print("4. Exit")
+        print("3. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
             data = input("Enter transaction data: ")
-            tree.add_transaction(data)
-            print("\nTransaction added to the Merkle tree!\n")
+            blockchain.add_transaction(data)  # Add the transaction to a transaction pool
+            print("\nTransaction added to the transaction pool!\n")
         elif choice == "2":
-            for block in blockchain.chain:
-                print("Index:", block.index)
-                print("Block Hash:", block.hash)
-                print("Previous Hash:", block.previous_hash)
-                print("Timestamp:", block.timestamp)
-                print("Transactions (Merkle Root):", block.transactions)
-                print()
+            blockchain.mine()  # Mine a new block with transactions from the pool
+            blockchain.view_chain()
         elif choice == "3":
-            if blockchain.verify_merkle_root(tree.build_tree()):
-                print("Blockchain is verified and valid.")
-            else:
-                print("Blockchain verification failed. It may have been tampered with.")
-        elif choice == "4":
             break
 
-if __name__ == "__main__":
+if name == "main":
     main()
